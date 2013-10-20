0a3e5cb7b123a0b2cfc23936101a8d6d4b818620
20947214 <20947214@student.uwa.edu.au>
2013-10-20 19:58:01 +0800

    register

---
+++ Website/PHP Pages/register_user.php
@@ -0,0 +1,21 @@
+<?php
+//Connecting to sql db.
+$connection = mysqli_connect('ec2-54-252-239-151.ap-southeast-2.compute.amazonaws.com', 'root');
+mysqli_select_db($connection, "centrelink");
+//Sending form data to sql db.
+session_start();
+$staffID = $_SESSION['staffID'];
+$pw = $_POST['Password_register'];
+$fn = $_POST['FirstName'];
+$ln = $_POST['LastName'];
+$em = $_POST['Email'];
+$st = $_POST['StreetAddress'];
+$sub = $_POST['Suburb'];
+$pc = $_POST['Postcode'];
+$sta = $_POST['searchstate'];
+$ph = $_POST['Phone'];
+mysqli_query($connection,"INSERT INTO staff (staffID, password, firstName, lastName, email, streetAddress, suburb, postcode, state, phone) VALUES ('$staffID', '$pw', '$fn', '$ln', '$em', '$st', '$sub', '$pc', '$sta', '$ph')
+ON DUPLICATE KEY UPDATE staffID = $staffID, password = '$pw', firstName = '$fn', lastName = '$ln', email = '$em', streetAddress = '$st', suburb = '$sub', postcode = '$pc', state = '$sta', phone = '$ph'") or die(mysql_error());
+mysqli_close();
+header('Location: index.html')
+?>
