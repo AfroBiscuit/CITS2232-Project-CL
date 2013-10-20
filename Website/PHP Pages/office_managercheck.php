60a0a210313d214b55e90128d7ddd7e443a47eb7
AfroBiscuit <cockman2@gmail.com>
2013-10-20 17:51:54 +0800

    small changes

--- Website/PHP Pages/office_managercheck.php
+++ Website/PHP Pages/office_managercheck.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 $_SESSION['officeID'] = $_POST['officeCode'];
-$officeID = $_SESSION['officeID];
-connection = mysqli_connect('ec2-54-252-239-151.ap-southeast-2.compute.amazonaws.com', 'root');
+$officeID = $_SESSION['officeID'];
+$connection = mysqli_connect('ec2-54-252-239-151.ap-southeast-2.compute.amazonaws.com', 'root');
 mysqli_select_db($connection, "centrelink");
-$query = "SELECT manager FROM offices WHERE officeCode=$officeID";
+$query = "SELECT officeManager FROM offices WHERE officeCode=$officeID";
 $result = mysqli_query($connection, $query);
 $row = mysqli_fetch_array($result,MYSQLI_NUM);
 if ($row[0]==$_SESSION['logID']) {header ('Location: office_manager.php');}
