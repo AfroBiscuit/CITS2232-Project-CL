d1ecade6b527e48aedfc4e540504f0a8392feb1b
20947214 <20947214@student.uwa.edu.au>
2013-10-20 18:01:54 +0800

    manager check

--- Website/PHP Pages/office_managercheck.php
+++ Website/PHP Pages/office_managercheck.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 $_SESSION['officeID'] = $_POST['officeCode'];
-$officeID = $_SESSION['officeID];
-connection = mysqli_connect('ec2-54-252-239-151.ap-southeast-2.compute.amazonaws.com', 'root');
+$officeID = $_SESSION['officeID'];
+$connection = mysqli_connect('ec2-54-252-239-151.ap-southeast-2.compute.amazonaws.com', 'root');
 mysqli_select_db($connection, "centrelink");
-$query = "SELECT manager FROM offices WHERE officeCode=$officeID";
+$query = "SELECT isManager FROM memberships WHERE officeCode=$officeID and staffID=$_SESSION['logID']";
 $result = mysqli_query($connection, $query);
 $row = mysqli_fetch_array($result,MYSQLI_NUM);
-if ($row[0]==$_SESSION['logID']) {header ('Location: office_manager.php');}
+if ($row[0]==1) {header ('Location: office_manager.php');}
 else {header ('Location: office.php');}
 mysqli_close($connection);
 ?>
