2e00b98b8e5b044c496a1cc5aa57065798cf5818
20947214 <20947214@student.uwa.edu.au>
2013-10-20 20:12:17 +0800

    register

--- Website/PHP Pages/register_user.php
+++ Website/PHP Pages/register_user.php
@@ -3,6 +3,7 @@
 $connection = mysqli_connect('ec2-54-252-239-151.ap-southeast-2.compute.amazonaws.com', 'root');
 mysqli_select_db($connection, "centrelink");
 //Sending form data to sql db.
+start_session();
 $query = "SELECT MAX(staffID) FROM staff";
 $result = mysqli_query($connection, $query);
 $row = mysqli_fetch_array($result,MYSQLI_NUM);
@@ -18,6 +19,8 @@ $sta = $_POST['searchstate'];
 $ph = $_POST['Phone'];
 mysqli_query($connection,"INSERT INTO staff (staffID, password, firstName, lastName, email, streetAddress, suburb, postcode, state, phone) VALUES ('$staffID', '$pw', '$fn', '$ln', '$em', '$st', '$sub', '$pc', '$sta', '$ph')
 ON DUPLICATE KEY UPDATE staffID = $staffID, password = '$pw', firstName = '$fn', lastName = '$ln', email = '$em', streetAddress = '$st', suburb = '$sub', postcode = '$pc', state = '$sta', phone = '$ph'") or die(mysql_error());
+$_SESSION['logID'] = $staffID;
+$_SESSION['staffID'] = $_SESSION['logID'];
+header('Location: details.php');
 mysqli_close();
-header('Location: index.html')
 ?>
