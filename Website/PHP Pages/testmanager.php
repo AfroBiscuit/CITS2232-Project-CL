acbee0db55a05743947a9aa195ee58a1f7539c57
AfroBiscuit <cockman2@gmail.com>
2013-10-20 19:49:18 +0800

    fix redirect

--- Website/PHP Pages/testmanager.php
+++ Website/PHP Pages/testmanager.php
@@ -1,10 +1,16 @@
 <?php
 $connection = mysqli_connect('ec2-54-252-239-151.ap-southeast-2.compute.amazonaws.com', 'root');
 mysqli_select_db($connection, "centrelink");
-$query = "SELECT isManager, staffID FROM memberships";
+session_start();
+$officeID = $_SESSION['officeID'];
+$logID = $_SESSION['logID'];
+echo $officeID;
+echo $logID;
+$query = "SELECT isManager FROM memberships WHERE officeCode='$officeID' and staffID = '$logID'";
+$result = mysqli_query($connection, $query);
 echo "<p>";
 while($row = mysqli_fetch_array($result,MYSQLI_NUM)) {
-echo $row[0], $row[1];
+echo $row[0];
 }
 echo "</p>";
 ?>
